92b158dda1423dcfb18273cbcdae3ca0b1519a6a
PatrykKobylarczyk <patryk.kobylarczyk@gmail.com>
2022-05-24 21:12:54 +0200

    Flashcard with Logo

---
+++ src/components/Flashcard/Flashcard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Flashcard.scss'
+import Logo from '../Logo/Logo';
+
+const Flashcard = () => {
+    return ( 
+        <div className="card">
+            <Logo/>
+        </div>
+     );
+}
+ 
+export default Flashcard;
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Logo.scss'
+
+const Logo = () => {
+    return (
+        <div className="logo">
+            <h3>fish<span>ky</span></h3>
+        </div>
+
+    );
+}
+
+export default Logo;
