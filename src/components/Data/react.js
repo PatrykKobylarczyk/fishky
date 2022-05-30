1612c89dac908ed6b0366e79f5bac02defb6e4bd
PatrykKobylarczyk <patryk.kobylarczyk@gmail.com>
2022-05-30 23:55:40 +0200

    added new words to arrays

--- src/components/Data/react.js
+++ src/components/Data/react.js
@@ -4,19 +4,19 @@ export const react = [
         word: 'Co to są komponenty wyższego rzędu?',
         description: 'Komponenty wyższego rzędu (ang. Higher Order Component - HOC), zasada jest podobna do funkcji wyższego rzędu. Komponenty tego typu do funkcje, które przyjmują komponenty jako argument oraz zwracają nowy argument. Przykładem może być funkcja connect z biblioteki Redux.'
     },
-    // {
-    //     id: 2,
-    //     word:'Jak działa Wirtualny DOM?',
-    //     description: 'Wirtualny DOM jest to reprezentacja (w pamięci) prawdziwego drzewa DOM. Operacje wykonywane są na Wirtualnym DOM i gdy coś się zmieni, wykonywane jest porównywanie drzew (ang. diff), a następnie najmniejsza liczba akcji potrzebna do tego aby oba drzewa były takie same. Algorytm, który używa React w celu uaktualnienia natywnego DOM, nazywany jest po angielsku reconciliation, co można przetłumaczyć jako pojednanie. Więcej o algorytmie możesz przeczytać w artykule z oficjalnej dokumentacji'
-    // },
-    // {
-    //     id: 3,
-    //     word:'What is Semantic HTML? and how does it work?',
-    //     description: 'Semantic HTML is a type of coding. </br> It is the use of HTML markup to emphasize the contents semantics or meaning.</br> Consider the following scenario: The <b></b> tag is not used for bold statements in semantic HTML, while the <i></i> element is used for italic.</br> Instead, you use the <em></em> and <strong></strong> tags.'
-    // },
-    // {
-    //     id: 4,
-    //     word:'Explain Box Sizing Property',
-    //     description: 'Box Sizing property defines how the height and width of a box are calculated. Content Box - The default width and height apply only to the element’s content. Padding and border are added outside the box. Padding Box - You add the dimensions to both element’s content and padding. It adds the border outside the box. Border Box - The dimensions are added to the content, padding, and border. '
-    // }
+    {
+        id: 2,
+        word:'Jak działa Wirtualny DOM?',
+        description: 'Wirtualny DOM jest to reprezentacja (w pamięci) prawdziwego drzewa DOM. Operacje wykonywane są na Wirtualnym DOM i gdy coś się zmieni, wykonywane jest porównywanie drzew (ang. diff), a następnie najmniejsza liczba akcji potrzebna do tego aby oba drzewa były takie same. Algorytm, który używa React w celu uaktualnienia natywnego DOM, nazywany jest po angielsku reconciliation, co można przetłumaczyć jako pojednanie. Więcej o algorytmie możesz przeczytać w artykule z oficjalnej dokumentacji'
+    },
+    {
+        id: 3,
+        word:'What is Semantic HTML? and how does it work?',
+        description: 'Semantic HTML is a type of coding. </br> It is the use of HTML markup to emphasize the contents semantics or meaning.</br> Consider the following scenario: The <b></b> tag is not used for bold statements in semantic HTML, while the <i></i> element is used for italic.</br> Instead, you use the <em></em> and <strong></strong> tags.'
+    },
+    {
+        id: 4,
+        word:'Explain Box Sizing Property',
+        description: 'Box Sizing property defines how the height and width of a box are calculated. Content Box - The default width and height apply only to the element’s content. Padding and border are added outside the box. Padding Box - You add the dimensions to both element’s content and padding. It adds the border outside the box. Border Box - The dimensions are added to the content, padding, and border. '
+    }
 ]
